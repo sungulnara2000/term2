fa367778a9fa7cdb36045d2df2488d78ea030e50
sungatullina gulnara gabdullovna <RusSunGab008>
2019-03-03 14:19:00 +0300

    update: add CountAddEdges function

---
+++ term2/task1/6_strongly_connected/CountAddEdges.cpp
@@ -0,0 +1,32 @@
+//
+// Created by gulnara on 03.03.19.
+//
+
+#include "list_graph.h"
+#include <algorithm>
+
+int CListGraph::CountAddEdges() {
+    vector<int> SCCsInDegree(components.size(), 0);
+    vector<int> SCCsOutDegree(components.size(), 0);
+    for (int i = 0; i < adjacencyList.size(); ++i) {
+        for (int j : adjacencyList[i]) {
+            if (colors[i] != colors[j]) {
+                SCCsOutDegree[colors[i]]++;
+                SCCsInDegree[colors[j]]++;
+            }
+        }
+    }
+    int X = 0;
+    int Y = 0;
+    for (int i : SCCsInDegree) {
+        if (i == 0) {
+            X++;
+        }
+    }
+    for (int i : SCCsOutDegree) {
+        if (i == 0) {
+            Y++;
+        }
+    }
+    return (std::max(X, Y));
+}
