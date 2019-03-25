1de2b30cb6a1d3b059291c6b55fbaae0c750b083
sungatullina gulnara gabdullovna <sungulnara2000@mail.ru>
2019-03-25 13:17:31 +0300

    get rid off odd files

--- term2/task1/2_shortest_cycle/main.cpp
+++ term2/task1/2_shortest_cycle/main.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
 #include "SetGraph.h"
 #include "shortestCycle.h"
+#include <vector>
+#include <iostream>
+#include <queue>
+
+#include "SetGraph.h"
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::queue;
+
+int vertexCycle(int vertex, const IGraph* graph) {
+
+    vector<bool> wasUsed(graph->VerticesCount(), false);
+    wasUsed[vertex] = true;
+
+    vector<int> prev(graph->VerticesCount(), -1);
+    vector<int> depth(graph->VerticesCount(), 0);
+
+    queue<int> bufferQueue;
+    bufferQueue.push(vertex);
+
+    while(!bufferQueue.empty()) {
+        int current = bufferQueue.front();
+        bufferQueue.pop();
+        vector<int> next;
+        graph->GetNextVertices(current, next);
+        for (int nextVertex : next) {
+            if (!wasUsed[nextVertex]) {
+                bufferQueue.push(nextVertex);
+                wasUsed[nextVertex] = true;
+                prev[nextVertex] = current;
+                depth[nextVertex] = depth[current] + 1;
+            } else if (prev[current] != nextVertex) {
+                return depth[nextVertex] + depth[current] + 1;
+            }
+        }
+    }
+    return graph->VerticesCount() + 1;
+}
+
+int shortestCycle(const IGraph* graph) {
+    int rv = graph->VerticesCount() + 1;
+    for (int i = 0; i < graph->VerticesCount(); ++i) {
+        rv = std::min(vertexCycle(i, graph), rv);
+    }
+    return rv;
+}
 
 int main() {
     int vertexCount, edgesCount;
