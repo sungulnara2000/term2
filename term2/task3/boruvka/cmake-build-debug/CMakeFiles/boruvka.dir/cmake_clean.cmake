4a3c8ff18d497a85a5c6706fac45260b21efeaf5
sungatullina gulnara gabdullovna <sungulnara2000@mail.ru>
2019-04-14 16:05:19 +0300

    add boruvka

---
+++ term2/task3/boruvka/cmake-build-debug/CMakeFiles/boruvka.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boruvka.dir/main.cpp.o"
+  "CMakeFiles/boruvka.dir/ListGraph.cpp.o"
+  "CMakeFiles/boruvka.dir/MSTweight.cpp.o"
+  "boruvka.pdb"
+  "boruvka"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boruvka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ term2/task3/boruvka/main.cpp
@@ -0,0 +1,18 @@
+#include "ListGraph.h"
+
+int MSTweight(ListGraph& graph);
+
+int main() {
+    int n;
+    int m;
+    cin >> n >> m;
+    ListGraph graph(n);
+    for (int i = 0; i < m; ++i) {
+        int from, to;
+        long weight;
+        cin >> from >> to >> weight;
+        graph.AddEdge(from - 1, to - 1, weight);
+    }
+    cout << MSTweight(graph);
+    return 0;
+}
