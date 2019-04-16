ad3d34410685d4441a8e2daceb34616cad689602
sungatullina gulnara gabdullovna <sungulnara2000@mail.ru>
2019-04-16 11:58:46 +0300

    using set instead of vector

--- term2/task2/1_cities/dijkstra.cpp
+++ term2/task2/1_cities/dijkstra.cpp
@@ -16,38 +16,31 @@ using std::distance;
 using std::pair;
 using std::cout;
 using std::endl;
-
-
-int MinDistVertex(vector<int>& dist, vector<bool>& SPT) {
-    int min = INT_MAX;
-    int min_index;
-    for (int i = 0; i < dist.size(); ++i) {
-        if (SPT[i] == false && dist[i] <= min) {
-            min = dist[i];
-            min_index = i;
-        }
-    }
-    return min_index;
-}
+using std::make_pair;
 
 int dijkstra(IGraph const &graph, int from, int to) {
     vector<int> dist(graph.VerticesCount(), INT_MAX);
     dist[from] = 0;
 
-    vector<bool> SPT(graph.VerticesCount(), false);
-    for (int j = 0; j < graph.VerticesCount() - 1; ++j) {
-//        auto min_elem = min_element(dist.begin(), dist.end()), [SPT](int a, int b) { return SPT.find(a)==SPT.end() && SPT.find(b)==SPT.end() && a < b; }) ;
+    vector<bool> used(graph.VerticesCount(), false);
 
-        int min_dist_vertex = MinDistVertex(dist, SPT);
-        SPT[min_dist_vertex] = true;
+    set<pair<int, int>> queue;
+    while(!queue.empty()) {
+        int min_dist_vertex = queue.begin()->second;
+        queue.erase(queue.begin());
+        used[min_dist_vertex] = true;
         vector<pair<int, int>> adjacents;
         graph.GetAdjacentVertices(min_dist_vertex, adjacents);
         for (auto i: adjacents) {
-            if (!SPT[i.first] && dist[i.first] > dist[min_dist_vertex] + i.second) {
+            if (dist[i.first] > dist[min_dist_vertex] + i.second) {
+                if (dist[i.first] != INT_MAX) {
+                    queue.erase(make_pair(dist[i.first], i.first));
+                }
                 dist[i.first] = dist[min_dist_vertex] + i.second;
+                queue.emplace(make_pair(dist[i.first], i.first));
             }
         }
     }
 
-    return dist[to];
+    return dist[to] != INT_MAX ? dist[to] : -1;
 }
